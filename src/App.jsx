02615f2c3602480606e484cb8bffbf1138203138
pushpakkolya <kolyapushpak70@gmail.com>
2026-02-13 00:50:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
+import Landing from "./rooms/Landing";
 import RoomOne from "./rooms/RoomOne";
 import RoomTwo from "./rooms/RoomTwo";
 
 function App() {
-  const [room, setRoom] = useState(1);
+  const [room, setRoom] = useState(0);
 
   return (
     <>
+      {room === 0 && <Landing onStart={() => setRoom(1)} />}
       {room === 1 && <RoomOne onComplete={() => setRoom(2)} />}
       {room === 2 && <RoomTwo onComplete={() => setRoom(3)} />}
     </>
