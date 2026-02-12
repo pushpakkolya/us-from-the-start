d066b4fbe7fd266420dccae1b00bf7c6648d61b9
pushpakkolya <kolyapushpak70@gmail.com>
2026-02-13 00:46:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import { useState, useEffect } from "react";
-import RoomOne from ".rooms/RoomOne";
-import RoomTwo from ".rooms/RoomTwo";
+import { useState } from "react";
+import RoomOne from "./rooms/RoomOne";
+import RoomTwo from "./rooms/RoomTwo";
 
 function App() {
   const [room, setRoom] = useState(1);
-
-  useEffect(() => {
-    const audio = document.getElementById("bg-music");
-
-    const startMusic = async () => {
-      try {
-        await audio.play();
-      } catch (err) {}
-    };
-
-    document.addEventListener("click", startMusic, { once: true });
-
-    return () => {
-      document.removeEventListener("click", startMusic);
-    };
-  }, []);
 
   return (
     <>
